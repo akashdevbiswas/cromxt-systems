9800d0e8071b4a8bb70dd726fd6bac9a13779e32
Akash Biswas <akashbiswas.271999@gmail.com>
2025-03-10 12:48:11 +0530

    Done some refactoring on the directory creation of the files.

--- bucket/src/main/java/com/cromxt/bucket/BucketApplication.java
+++ bucket/src/main/java/com/cromxt/bucket/BucketApplication.java
@@ -27,28 +27,15 @@ public class BucketApplication {
             String basePath = environment.getProperty("BUCKET_CONFIG_DISK_PATH", String.class);
             assert basePath != null;
 
-            File privateDirectory = new File(basePath + File.separator + FileVisibility.PRIVATE_ACCESS.getAccessType());
-            if (!privateDirectory.exists()) {
-                if (!privateDirectory.mkdirs()) {
-                    log.error("Unable to create the private directory");
-                    SpringApplication.exit(context, () -> 1);
+            for (FileVisibility visibility : FileVisibility.values()) {
+                File directory = new File(basePath + File.separator + visibility.getAccessType());
+                if (!directory.exists()) {
+                    if (!directory.mkdirs()) {
+                        log.error("Unable to create the public directory");
+                        SpringApplication.exit(context, () -> 1);
+                    }
                 }
-            }
 
-            File publicDirectory = new File(basePath + File.separator + FileVisibility.PUBLIC_ACCESS.getAccessType());
-            if (!publicDirectory.exists()) {
-                if (!publicDirectory.mkdirs()) {
-                    log.error("Unable to create the public directory");
-                    SpringApplication.exit(context, () -> 1);
-                }
-            }
-
-            File protectedDirectory = new File(basePath + File.separator + FileVisibility.PROTECTED_ACCESS.getAccessType());
-            if (!protectedDirectory.exists()) {
-                if (!protectedDirectory.mkdirs()) {
-                    log.error("Unable to create the protected directory");
-                    SpringApplication.exit(context, () -> 1);
-                }
             }
 
         };
