82b7bcbe4e067d6546552a09f5f71b969bb0051b
Akash Biswas <akashbiswas.271999@gmail.com>
2025-03-29 00:02:33 +0530

    Add all the modules to the crombucket main maven module, so some issues created.

--- server/storage-cluster-router/src/main/java/com/cromxt/clusterrouter/service/ClusterRouterService.java
+++
@@ -1,9 +0,0 @@
-package com.cromxt.clusterrouter.service;
-
-import com.cromxt.common.crombucket.routeing.StorageServerAddress;
-import com.cromxt.common.crombucket.routeing.MediaDetails;
-import reactor.core.publisher.Mono;
-
-public interface ClusterRouterService {
-    Mono<StorageServerAddress> getBucketDetails(MediaDetails mediaDetails);
-}
