9a191f2f4673aaaebffaf7452619c016ac8454d8
Akash Biswas <akashbiswas.271999@gmail.com>
2025-04-13 16:07:43 +0530

    Setup the sprin-security in the Reactive context.

--- crombucket-server/user-service/src/main/java/com/cromxt/crombucket/config/ApplicationConfig.java
+++ crombucket-server/user-service/src/main/java/com/cromxt/crombucket/config/ApplicationConfig.java
@@ -1,26 +1,33 @@
 package com.cromxt.crombucket.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import reactor.core.publisher.Mono;
 
 @Configuration
+@Slf4j
 public class ApplicationConfig {
 
-
     @Bean
-    public ReactiveUserDetailsService userDetailsService() {
+    public ReactiveUserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
         return new ReactiveUserDetailsService() {
             @Override
             public Mono<UserDetails> findByUsername(String username) {
-                return null;
+                log.error("Called user details service {}.",username);
+
+                return Mono.just(User.builder()
+                                .username("akash")
+                                .password(passwordEncoder.encode("password"))
+                        .build());
             }
         };
     }
